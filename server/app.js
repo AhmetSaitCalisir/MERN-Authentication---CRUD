7686fac544887ea0e45bda0f62d81fce891952d0
Ahmet Sait Çalışır <56252214+AhmetSaitCalisir@users.noreply.github.com>
2021-04-29 14:22:03 +0300

    v0.0.1

---
+++ server/app.js
@@ -0,0 +1,18 @@
+require("dotenv").config({ path: "./config.env" });
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res, next) => {
+  res.send("Api running");
+});
+
+const PORT = process.env.PORT || 4545;
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}/`);
+});
