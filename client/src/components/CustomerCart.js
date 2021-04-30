7a5e80058d36be7b28d2d6b542dcd275ff4d5dd8
Ahmet Sait Çalışır <56252214+AhmetSaitCalisir@users.noreply.github.com>
2021-05-01 00:33:11 +0300

    v0.1.3

---
+++ client/src/components/CustomerCart.js
@@ -0,0 +1,60 @@
+import { Link } from "react-router-dom";
+
+import {
+  EnvelopeFill,
+  PersonBadgeFill,
+  GeoAltFill,
+  GearFill,
+} from "react-bootstrap-icons";
+
+const CustomerCart = ({ customer }) => {
+  const colStyle = {
+    marginBottom: "5px",
+    marginTop: "5px",
+  };
+
+  return (
+    <div className="container-fluid" style={colStyle}>
+      <div className="row">
+        <div className="col">
+          <div className="card">
+            <div className="card-header">
+              <GeoAltFill /> {customer.city}/{customer.district}
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">{customer.companyName}</h5>
+              <p className="card-text">
+                <PersonBadgeFill /> {customer.managerName}
+              </p>
+              <p className="card-text">
+                <a href={"mailto:" + customer.email}>
+                  <EnvelopeFill /> {customer.email}
+                </a>
+              </p>
+              <div className="row">
+                <div className="col">
+                  <Link
+                    to="/customer"
+                    className="btn btn-outline-warning btn-sm btn-block"
+                  >
+                    <GearFill /> Update
+                  </Link>
+                </div>
+                <div className="col">
+                  <Link
+                    to="/customer"
+                    className="btn btn-outline-danger btn-sm btn-block"
+                  >
+                    <GearFill /> Delete
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CustomerCart;
--- client/src/pages/Customers.js
+++ client/src/pages/Customers.js
@@ -1,9 +1,61 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import CustomerCart from "../components/CustomerCart";
 
-const Customers = () => {
-  return (
-    <div>
-      <h1>Customers</h1>
+const Customers = ({ history }) => {
+  const [error, setError] = useState("");
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    const fetchCustomersData = async () => {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+        },
+      };
+
+      try {
+        const { data } = await axios.get(
+          "http://localhost:4545/api/customer",
+          config
+        );
+        console.log(data);
+        setCustomers(data.data);
+      } catch (error) {
+        console.log(error);
+        localStorage.removeItem("authToken");
+        localStorage.removeItem("username");
+        setError("You are not authorized please login");
+        history.push("/login");
+      }
+    };
+
+    fetchCustomersData();
+  }, [history]);
+
+  return error ? (
+    <div
+      className="container
+      "
+    >
+      <div className="row">
+        <div className="col">
+          <div className="alert alert-danger" role="alert">
+            {error}
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="container">
+      <div className="row">
+        {customers.map((customer) => (
+          <div className="col" key={customer._id}>
+            <CustomerCart customer={customer} key={customer._id} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
